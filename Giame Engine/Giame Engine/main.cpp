2d71981cf8c557a4e2c9369d86a6d28ddf88dee0
LinWeifeng <linweifeng.gm@bytedance.com>
2020-07-26 00:44:53 +0800

    新建工程

---
+++ Giame Engine/Giame Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Giame Engine
+//
+//  Created by bytedance on 2020/7/26.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
